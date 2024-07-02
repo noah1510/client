d19816d7dd3b4b5d437979fb0f2d9107b20c7f2f
Noa Sakurajin <14131388+noah1510@users.noreply.github.com>
2024-07-02 15:27:05 +0200

    moving remaining assets to default_assets and updating all the paths

--- ui/game_ui.gd
+++ ui/game_ui.gd
@@ -1,1 +1,11 @@
 extends Node
+
+
+func _ready() -> void:
+    if Config.is_dedicated_server:
+        return
+
+    # Todo get the current player and set the icons to the
+    # the ones for the actual character of the current player
+
+    pass
