a7588a7010b5e63ba6c0c073ddcfc27bf0aed461
noasakurajin <dev@noasakurajin.de>
2024-08-03 11:47:09 +0200

    update headless launcher

--- .github/actions/setup_headless_display/xvfb_launch.sh
+++ .github/actions/setup_headless_display/xvfb_launch.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
 # Use xvfb-run to run the command unless RUNNER_OS is Windows
-if [ "$RUNNER_OS" == "Windows" ]; then
-    $@
-else
+if [ "$RUNNER_OS" == "Linux" ]; then
     export LIBGL_ALWAYS_SOFTWARE="true"
     export GALLIUM_DRIVER="llvmpipe"
     export MESA_VK_DEVICE_SELECT="llvmpipe"
-    export DISPLAY=":42"
     
     xvfb-run $@
+else
+    $@
 fi
